e0b550711da1d948a3b1bfb7a96e990a5f2a598a
hobit22 <hobit22@naver.com>
2022-04-11 16:58:30 +0900

    * PlanController 수정 RestController로 변경

--- src/main/java/com/hanghea/list99/controller/PlanController.java
+++ src/main/java/com/hanghea/list99/controller/PlanController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PlanController {
 
