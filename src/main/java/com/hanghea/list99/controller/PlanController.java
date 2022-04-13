8dedf40a1298866f91918e6ebda50b4ad3ae7de1
hobit22 <hobit22@naver.com>
2022-04-13 17:04:34 +0900

    * Plan Controller Return Fix POST, DELETE, PUT, PATCH Mapping Return 값 수정 기존 Long -> 수정후 ResponseEntity<String>

--- src/main/java/com/hanghea/list99/controller/PlanController.java
+++ src/main/java/com/hanghea/list99/controller/PlanController.java
@@ -29,11 +29,11 @@ public class PlanController {
             @RequestParam("sortBy") String sortBy,
             @RequestParam("isAsc") boolean isAsc,
             @AuthenticationPrincipal UserDetailsImpl userDetails
-    ){
+    ) {
         Long userId = userDetails.getUser().getId();
-        page = page - 1 ;
+        page = page - 1;
 
-        Page<Plan> target =planService.getPlans(userDetails.getUser(), page, size, sortBy, isAsc);
+        Page<Plan> target = planService.getPlans(userDetails.getUser(), page, size, sortBy, isAsc);
 
         return target;
     }
@@ -44,18 +44,24 @@ public class PlanController {
 
     //plan 등록 테스트
     @PostMapping("/api/plan")
-    public ResponseEntity<String> createPlans(@RequestBody PlanDto.Request request, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<String> createPlans(@RequestBody PlanDto.Request request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         User user = userDetails.getUser();
         planService.createPlans(request, user);
         return ResponseEntity.ok().body("plan 등록 완료");
     }
+
     // plan 수정
     @PutMapping("/api/plan/{planId}")
-    public Long updatePlan(@PathVariable Long planId, @RequestBody PlanDto.Request request) {
+//    public Long updatePlan(@PathVariable Long planId, @RequestBody PlanDto.Request request) {
+//        planService.update(planId, request);
+//        return planId;
+//    }
+    public ResponseEntity<String> updatePlan(@PathVariable Long planId, @RequestBody PlanDto.Request request) {
         planService.update(planId, request);
-        return planId;
+        return ResponseEntity.ok().body("plan 수정 완료!");
     }
+
     // 체크 박스 흠.. 잘모르겠음. 도움이 필요해요 ㅠㅠ
 //    @PutMapping("/api/plan/{planId}/status")
 //    public Long checkPlan(@PathVariable Long planId, @RequestBody PlanDto.Response response) {
@@ -64,23 +70,40 @@ public class PlanController {
 //    }
     //포스트맨에서 테스트 시 ,
     @PatchMapping("/api/plan/{planId}/status")
-    public Boolean checkPlan(@PathVariable Long planId) {
+    public ResponseEntity<String> checkPlan(@PathVariable Long planId) {
         Plan p = planService.check(planId);
-        if (p.getStatus() == false){
+        if (p.getStatus() == false) {
             p.setStatus(true);
         } else {
             p.setStatus(false);
         }
         planRepository.save(p);
-        return p.getStatus();
+        return ResponseEntity.ok().body("plan 상태 변경 완료!");
     }
+//    public Boolean checkPlan(@PathVariable Long planId) {
+//        Plan p = planService.check(planId);
+//        if (p.getStatus() == false) {
+//            p.setStatus(true);
+//        } else {
+//            p.setStatus(false);
+//        }
+//        planRepository.save(p);
+//        return p.getStatus();
+//    }
 
 
     // plan 삭제
+//    @DeleteMapping("/api/plan/{planId}")
+//    public Long deletePlan(@PathVariable Long planId) {
+//        planRepository.deleteById(planId);
+//        return planId;
+//    }
+
+
     @DeleteMapping("/api/plan/{planId}")
-    public Long deletePlan(@PathVariable Long planId) {
+    public ResponseEntity<String> deletePlan(@PathVariable Long planId) {
         planRepository.deleteById(planId);
-        return planId;
+        return ResponseEntity.ok().body("게시물 삭제 완료!");
     }
 
 
